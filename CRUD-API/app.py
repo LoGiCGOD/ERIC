40403c61192fe99a761667717e3bc472c2745b2f
Avishek Dutta <79700628+LoGiCGOD@users.noreply.github.com>
2024-04-29 10:11:38 +0530

    Update app.py

--- CRUD-API/app.py
+++ CRUD-API/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson import ObjectId
 import os
-
+##hi
 app = Flask(__name__)
 
 # Use Kubernetes Secrets to get MongoDB credentials
